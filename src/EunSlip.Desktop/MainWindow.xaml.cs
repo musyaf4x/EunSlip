65d9021b5bc6569ea1047b852e09761481000f29
hafidzmusyafa <hafidzmusyafa@student.telkomuniversity.ac.id>
2026-07-22 20:23:05 +0700

    fix: localize sending close dialog

--- src/EunSlip.Desktop/MainWindow.xaml.cs
+++ src/EunSlip.Desktop/MainWindow.xaml.cs
@@ -2,6 +2,7 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Markup;
+using EunSlip.Desktop.Localization;
 using EunSlip.Desktop.ViewModels;
 
 namespace EunSlip.Desktop;
@@ -25,8 +26,8 @@ public partial class MainWindow : Window
 
         e.Cancel = true;
         _ = MessageBox.Show(
-            "Pengiriman sedang berlangsung. Tunggu sampai proses selesai.",
-            "EunSlip",
+            Strings.Get("SendingInProgress"),
+            Strings.Get("AppName"),
             MessageBoxButton.OK,
             MessageBoxImage.Information);
     }
